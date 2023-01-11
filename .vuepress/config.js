66c6d0f0dcd643e48a232f58cb417487455753a8
NIKON <nikon@NIKONs-MacBook-Air.local>
2023-01-11 14:04:12 +0900

    add

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,15 @@
 module.exports = {
   title: 'HITOWA',
   description:'HITOWA GROUP MESSAGE',
-
   themeConfig: {
     navbar: false
   },
   patterns: [
-    'docs/*.md',
-    'docs/**/*.md',
-    '.vupress/**/*.vue',
-    '!amplify/**',
-    '!tools/**'
+    // 'docs/*.md',
+    // 'docs/**/*.md',
+    // '.vupress/**/*.vue',
+    // '!amplify/**',
+    // '!tools/**'
   ],
   postcss: {
     plugins: [

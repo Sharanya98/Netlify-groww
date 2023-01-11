badeb4f6fa77f205dbe68402badb9b622c4922bc
NIKON <nikon@NIKONs-MacBook-Air.local>
2023-01-11 16:45:47 +0900

    add

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,13 +6,5 @@ module.exports = {
       {text:'Home', link:'/'},
       {text:'categories', link:'/categories/'}
   ],
-  patterns: [
-    'docs/*.md',
-    'docs/**/*.md',
-    '.vupress/**/*.vue',
-    '!amplify/**',
-    '!tools/**'
-  ],
-  
   }
 }

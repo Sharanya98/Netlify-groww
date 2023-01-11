40c35aad7399426f69fe6076f0589f67dfff7381
NIKON <nikon@NIKONs-MacBook-Air.local>
2023-01-11 15:48:15 +0900

    add

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -13,6 +13,12 @@ module.exports = {
     '!amplify/**',
     '!tools/**'
   ],
+  postcss: {
+    plugins: [
+      require("autoprefixer"),
+      require("tailwindcss")("./tailwind.config.js")
+    ],
 
+  }
   }
 }
